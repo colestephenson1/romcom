6f10681414710ccf58267b429048c3cb61b87348
Thomas Fettig <thomasmfettig@gmail.com>
2022-05-22 17:57:43 -0600

    Refactor and organize

--- src/main.js
+++ src/main.js
@@ -1,35 +1,34 @@
 // Create variables targetting the relevant DOM elements here 👇
-
 var coverImage = document.querySelector('.cover-image');
 var coverTitle = document.querySelector('.cover-title');
 var tagline1 = document.querySelector('.tagline-1');
 var tagline2 = document.querySelector('.tagline-2');
+//Buttons:
 var homeButton = document.querySelector('.home-button');
 var randomCoverButton = document.querySelector('.random-cover-button');
 var saveCoverButton = document.querySelector('.save-cover-button');
 var viewSavedButton = document.querySelector('.view-saved-button');
 var makeNewButton = document.querySelector('.make-new-button');
+var customCoverButton = document.querySelector('.create-new-book-button');
+//Page Views:
 var formView = document.querySelector('.form-view');
 var viewHomeImage = document.querySelector('.home-view');
 var savedView = document.querySelector('.saved-view');
 var savedCoverSection = document.querySelector('.saved-covers-section');
+var homeView = document.querySelector('.home-view');
+//Form Inputs:
 var customCoverInput = document.querySelector('.user-cover');
 var customTitleInput  = document.querySelector('.user-title');
 var customDescrip1Input  = document.querySelector('.user-desc1');
 var customDescrip2Input = document.querySelector('.user-desc2');
-var customCoverButton = document.querySelector('.create-new-book-button');
-var homeView = document.querySelector('.home-view');
 
 // // We've provided a few variables below
-
 var savedCovers = [
-  new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
-];
+  new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")];
 
 var currentCover;
 
 // Add your event listeners here 👇
-
 window.addEventListener('load', makeRandomCover);
 randomCoverButton.addEventListener('click', makeRandomCover);
 makeNewButton.addEventListener('click', goToMakeCover);
@@ -75,7 +74,7 @@ function goToHome() {
 }
 
 function savedCoverImages() {
-  savedCoverSection.innerHTML = ""
+  savedCoverSection.innerHTML = "";
     for (i = 0; i < savedCovers.length; i++) {
     savedCoverSection.innerHTML += `<section class='mini-cover'>
         <img class= "cover-image" id= "${savedCovers[i].id}" src=${savedCovers[i].cover}>
@@ -89,15 +88,14 @@ function showSavedCoverSection() {
   savedCoverImages();
 }
 
-
 function goToSaved() {
-  viewHomeImage.classList.add('hidden')
-  formView.classList.add('hidden')
-  savedView.classList.remove('hidden')
+  viewHomeImage.classList.add('hidden');
+  formView.classList.add('hidden');
+  savedView.classList.remove('hidden');
   viewSavedButton.classList.remove('hidden');
   randomCoverButton.classList.add('hidden');
   homeButton.classList.remove('hidden');
-  saveCoverButton.classList.add('hidden')
+  saveCoverButton.classList.add('hidden');
 }
 
 function makeCustomCover(event) {
@@ -123,8 +121,8 @@ function saveCover() {
 function deleteSavedCover(event) {
  for (i = 0; i < savedCovers.length; i++) {
    if (event.target.id == savedCovers[i].id) {
-     savedCovers.splice(i, 1)
+     savedCovers.splice(i, 1);
    }
  }
- showSavedCoverSection()
+ showSavedCoverSection();
 }
